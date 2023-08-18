84a72aec36a80daba4b284ac08afcb03a8fcd5df
Yrova <yarovavaleriya@gmail.com>
2023-08-18 10:51:01 +0300

    add test testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void shouldCalculatCeashbackHack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldExpectedException() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
+    }
 }
