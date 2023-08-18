642cad2427e48dc3586875ddadb583aaf5fd3ffb
Yrova <yarovavaleriya@gmail.com>
2023-08-18 10:25:55 +0300

    add junit settings

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void shouldCalculatCeashbackHack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void shouldExpectedException() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
+    }
 }
